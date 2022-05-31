6f4d47169bcc51535be599fcf65a0aa52a9f8327
LBGletsoisa <105360000+LBGletsoisa@users.noreply.github.com>
2022-05-31 02:49:22 +0200

    Add files via upload

---
+++ S2TL.py
@@ -0,0 +1,8 @@
+import re
+from bs4 import BeautifulSoup
+import streamlit as st
+
+st.set_page_config(page_title="My Website", page_icon=":tada", layout="wide")
+st.subheader("Sensor 2")
+
+st.write("Sensor 2 in the basememt shaft control unit, has detetected temperatures lower than the threshold of 35℃")
