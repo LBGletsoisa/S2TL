d9f93e78817a9be5f45bc5aa403d8b804dff9cde
LBGletsoisa <105360000+LBGletsoisa@users.noreply.github.com>
2022-06-01 23:48:06 +0200

    Update S2TL.py

--- S2TL.py
+++ S2TL.py
@@ -5,4 +5,4 @@ import streamlit as st
 st.set_page_config(page_title="My Website", page_icon=":tada", layout="wide")
 st.subheader("Sensor 2")
 
-st.write("Sensor 2 in the basememt shaft control unit, has detetected temperatures lower than the threshold of 35℃")
+st.write("Sensor 2 in the basement shaft control unit, has detected temperatures lower than the threshold of 15℃")
